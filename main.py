2e8c87e8c47227a742e953568331ee03a8fe5b68
Josphat Muinde <muindejosphat@gmail.com>
2023-02-16 20:58:32 +0300

    Deleted python files added to the project folder by mistake

--- main.py
+++
@@ -1,9 +0,0 @@
-def rotate(nums, k:int):
-    result=[]
-    for i in range(-k,len(nums)):
-        result.append(nums[i])
-    #result.extend(nums[0:k])
-    return result
-
-if __name__=='__main__':
-    print(rotate([1,2,3,4,2,3,4],1))
